0f5bc21eee94daed958ae51a10a3c8231150a739
BM <chane81@naver.com>
2019-12-31 01:40:20 +0900

    토큰 + prefix 상수로 레디스값 가져오게 수정

--- src/modules/user/ConfirmUser.ts
+++ src/modules/user/ConfirmUser.ts
@@ -1,13 +1,14 @@
 import { Resolver, Mutation, Arg } from 'type-graphql';
 import { User } from '../../entity/User';
 import { redis } from '../../redis';
+import { confirmUserPrefix } from '../constants/redisPrefixes';
 
 @Resolver(User)
 export class ConfirmUserResolver {
   // MUTATION - 삽입, 삭제, 수정
   @Mutation(() => Boolean)
   async confirmUser(@Arg('token') token: string): Promise<boolean> {
-    const userId = await redis.get(token);
+    const userId = await redis.get(confirmUserPrefix + token);
 
     if (!userId) {
       return false;
